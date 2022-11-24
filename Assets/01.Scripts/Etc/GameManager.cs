0cfb4656b0a02b9113110ee94d228e1c016da910
pmj0704 <ggm.s210012@ggh.goe.go.kr>
2022-11-24 09:14:52 +0900

    SnowBall

--- Assets/01.Scripts/Etc/GameManager.cs
+++ Assets/01.Scripts/Etc/GameManager.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
-    
+    private int snows = 0;
+
+    public int getSnow
+    {
+        get => snows;
+    }
+
+    public void AddSnow(int amount)
+    {
+        snows += amount;
+    }    
 }
