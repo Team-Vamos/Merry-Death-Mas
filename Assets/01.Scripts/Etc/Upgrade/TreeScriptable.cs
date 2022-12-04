6db78c890398e602b416cff372dc0bdce2731cf9
kchanyoung303 <abv021021021@gmail.com>
2022-12-04 23:12:03 +0900

    레벨

--- Assets/01.Scripts/Etc/Upgrade/TreeScriptable.cs
+++ Assets/01.Scripts/Etc/Upgrade/TreeScriptable.cs
@@ -1,30 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu(fileName = "UpData", menuName = "Scriptable/Create UpData")]
+public class TreeScriptable : ScriptableObject
+{
+    public TreeUpgrade[] TreeUpgrade;
+}
 
+[System.Serializable]
+public class TreeUpgrade
+{
 
-    [CreateAssetMenu(fileName ="UpData",menuName ="Scriptable/Create UpData")]
-    public class TreeScriptable : ScriptableObject
-    {
-        public TreeUpgrade[] TreeUpgrade;
-    }
+    public string UpName;
+    public Sprite ItemImage;
+    public bool isMaxUp;
+    public int MaxUpgrade;
+    public UpgradeInfo[] UpgradeLevel;
+}
 
-    [System.Serializable]
-    public class TreeUpgrade
-    {
-
-        public string UpName;
-        public bool isMaxUp;
-        public int MaxUpgrade = 5;
-        public UpgradeInfo[] UpgradeLevel;
-    }
-
-    [System.Serializable]
-    public class UpgradeInfo
-    {
-      public int BuyCost;
-      public int value1;
-      public int value2;
-      public int value3;
-    }
-
+[System.Serializable]
+public class UpgradeInfo
+{
+    public int BuyCost;
+    public int value1;
+    public int value2;
+    public int value3;
+}
--- Assets/01.Scripts/Etc/Upgrade/UpgradeUI.cs
+++ Assets/01.Scripts/Etc/Upgrade/UpgradeUI.cs
@@ -3,91 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class UpgradeUI : MonoBehaviour
+{
+    public TreeScriptable UpgradeData;
+    public Image UpgradeImage;
 
-    public class UpgradeUI : MonoBehaviour
+    public Text ItemLevel, ItemName, UpgradeCostText;
+    public Button BuyButton;
+    [SerializeField]
+    private bool IsBuy;
+
+    private int value1, value2, value3;
+
+    public int currentIndex = 0;
+    public int currentLevel = 0;
+
+
+
+    private void Start()
     {
-        public TreeScriptable UpgradeData;
-        public Image UpgradeImage;
-        public Text ItemLevel, ItemName, UpgradeCostText;
-        public Button BuyButton;
-        [SerializeField]
-        private bool IsBuy;
+        UpdateValues();
+        UpdateLevelInfo();
+    }
 
-        private int value1, value2, value3;
-
-        public int currentIndex = 0;
-        public int currentLevel = 0;
-
-
-
-        private void Start()
+    private void Update()
+    {
+        if (currentLevel == UpgradeData.TreeUpgrade[currentIndex].MaxUpgrade)
         {
+            UpgradeCostText.text = "MAX".ToString();
+        }
+        Debug.Log("CurIndx: "+currentIndex + "        " + "CurLv: "+currentLevel+"     "+"Value1: "+value1);
+    }
 
+    public void NextLevel()
+    {
+
+        if (currentLevel < UpgradeData.TreeUpgrade[currentIndex].MaxUpgrade)
+        {
+            currentLevel++;
             UpdateValues();
             UpdateLevelInfo();
         }
+    }
 
-        private void Update()
+    void UpdateValues()
+    {
+        value1 = UpgradeData.TreeUpgrade[currentIndex].UpgradeLevel[currentLevel].value1;
+        value2 = UpgradeData.TreeUpgrade[currentIndex].UpgradeLevel[currentLevel].value2;
+        value3 = UpgradeData.TreeUpgrade[currentIndex].UpgradeLevel[currentLevel].value3;
+        if (currentLevel >= UpgradeData.TreeUpgrade[currentIndex].MaxUpgrade)
         {
-            if(currentLevel == UpgradeData.TreeUpgrade[currentIndex].MaxUpgrade)
-            {
-                UpgradeCostText.text = "MAX".ToString();
-                UpgradeData.TreeUpgrade[currentIndex].isMaxUp = true;
-                BuyButton.interactable = false;
-            }
-        }
 
-        public void NextLevel()
-        {
-            if (currentLevel < UpgradeData.TreeUpgrade[currentIndex].MaxUpgrade)
-            {
-                currentLevel++;
-                UpdateValues();
-                UpdateLevelInfo();
-            }
-        }
-
-        void UpdateValues()
-        {
-            value1 = UpgradeData.TreeUpgrade[currentIndex].UpgradeLevel[currentLevel].value1;
-            value2 = UpgradeData.TreeUpgrade[currentIndex].UpgradeLevel[currentLevel].value2;
-            value3 = UpgradeData.TreeUpgrade[currentIndex].UpgradeLevel[currentLevel].value3;
-        }
-
-        void UpdateLevelInfo()
-        {
-            ItemName.text = UpgradeData.TreeUpgrade[currentIndex].UpName;
+            BuyButton.interactable = false;
             ItemLevel.text = "Level : " + currentLevel;
-            UpgradeCostText.text = UpgradeData.TreeUpgrade[currentIndex].UpgradeLevel[currentLevel].BuyCost.ToString();
+            UpgradeData.TreeUpgrade[currentIndex].isMaxUp = true;
         }
+    }
 
-        public void CandyCane()
+    void UpdateLevelInfo()
+    {
+        UpgradeImage.sprite = UpgradeData.TreeUpgrade[currentIndex].ItemImage;
+        ItemName.text = UpgradeData.TreeUpgrade[currentIndex].UpName;
+        ItemLevel.text = "Level : " + currentLevel;
+        UpgradeCostText.text = UpgradeData.TreeUpgrade[currentIndex].UpgradeLevel[currentLevel].BuyCost.ToString();
+
+    }
+
+    public void CandyCane()
+    {
+        Debug.Log("Value 1 : " + value1);
+        if (currentLevel <= 0)
         {
-            Debug.Log("Value 1 : "+value1);
-            if (currentLevel <= 0)
-            {
-                Debug.Log("아이템 구매");
-
+            Debug.Log("아이템 구매");
             GameManager.Instance.MultiplyShovelDmg(value1);
             Debug.Log(GameManager.Instance.ShovelDmg);
-            }
-            else
-            {
+        }
+        else
+        {
             GameManager.Instance.MultiplyShovelDmg(value1);
             Debug.Log("캔디 강화");
             Debug.Log(GameManager.Instance.ShovelDmg);
         }
 
-        }
-
-        public void Turr(int value1)
-        {
-
-        }
-
-        public void Star()
-        {
-
-        }
     }
 
+    public void Turr(int value1)
+    {
+
+    }
+
+    public void Star()
+    {
+
+    }
+}
